de632a2ce723d8a701cbd25c6e8ce4d7cc4aff7c
cristopher92 <deidda.cristopher@gmail.com>
2023-04-02 16:19:36 +0200

    pid

--- lab unix/prova.c
+++ lab unix/prova.c
@@ -1,3 +1,9 @@
-int main(){
+#include "stdio.h"
+#include "unistd.h"
 
+int main(int argc, char *argv[]){
+    printf("test process id\n");
+
+    printf("id del processo %d\n", getpid());
+    printf("id del processo padre %d\n", getppid());
 }
